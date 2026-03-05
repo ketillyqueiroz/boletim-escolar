2fe96fa29fd968b15d597b01f5d36fc281bc4f8a
Ketilly Queiroz <ketillyunicsul@gmail.com>
2026-03-05 19:35:23 -0300

    Estrutura inicial do dashboard.

--- frontend/boletim-escolar/src/app/app.routes.ts
+++ frontend/boletim-escolar/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ReportCardComponent } from './components/report-card/report-card.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StudentsComponent } from './components/students/students.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'boletim', component: ReportCardComponent },
+  { path: 'students', component: StudentsComponent },
+];
